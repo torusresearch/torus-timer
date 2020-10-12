88963cefd7971fdf3619a61795d88883ddcb06d9
tetratorus <tetratorus@gmail.com>
2020-10-12 16:33:04 +0800

    Use cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express')
+var cors = require('cors')
 var app = express()
+app.use(cors())
 app.get('/', (req, res) => {
   var timeout = req.query.timeout
   if (timeout > 30000) {
